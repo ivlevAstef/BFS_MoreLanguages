b44480fc4156cbe5f3222d17eb2298ddf071eeac
kuviman <kuviman@gmail.com>
2020-12-31 00:22:24 +0400

    Separate hacky collections into array2d and queue

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,13 +1,13 @@
 // Because these warnings are annoying when testing different things
 #![allow(dead_code, unused_imports)]
 
-#[cfg_attr(feature = "hacky-collections", path = "array2d/hacky.rs")]
-#[cfg_attr(not(feature = "hacky-collections"), path = "array2d/normal.rs")]
+#[cfg_attr(feature = "hacky-array2d", path = "array2d/hacky.rs")]
+#[cfg_attr(not(feature = "hacky-array2d"), path = "array2d/normal.rs")]
 mod array2d;
 mod bfs;
 mod point;
-#[cfg_attr(feature = "hacky-collections", path = "queue/hacky.rs")]
-#[cfg_attr(not(feature = "hacky-collections"), path = "queue/normal.rs")]
+#[cfg_attr(feature = "hacky-queue", path = "queue/hacky.rs")]
+#[cfg_attr(not(feature = "hacky-queue"), path = "queue/normal.rs")]
 mod queue;
 
 use point::Point;
